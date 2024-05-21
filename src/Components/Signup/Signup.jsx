5ea1f10ef2e5a51c359e569d13bfb6b1eaba8493
mannumishra <mannumishra2207@outlook.com>
2024-05-21 12:30:02 +0530

    add chnages

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -92,116 +92,106 @@ const Signup = () => {
   return (
     <>
       <p className='signupheading'>Create New Account</p>
-      <div className="singnup">
+      <div className="container signup">
         <div className="signupform">
           <form action="" onSubmit={postData}>
             <div className="row g-3">
-              <div className="col mb-3">
-                <label htmlFor=""> Name  <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='name' className="form-control" onChange={getInputData} placeholder="Name" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="name"> Name <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="name" className="form-control" onChange={getInputData} placeholder="Name" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Father Name <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='fathername' className="form-control" onChange={getInputData} placeholder="Father Name" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="fathername">Father Name <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="fathername" className="form-control" onChange={getInputData} placeholder="Father Name" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Mother Name <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='mothername' className="form-control" onChange={getInputData} placeholder="Mother Name" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="mothername">Mother Name <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="mothername" className="form-control" onChange={getInputData} placeholder="Mother Name" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Age <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='age' className="form-control" onChange={getInputData} placeholder="Age" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="age">Age <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="age" className="form-control" onChange={getInputData} placeholder="Age" required />
               </div>
-            </div>
-            <div className="row g-3">
-              <div className="col mb-3">
-                <label htmlFor="">Gender <span><sup className='text-danger'>*</sup></span></label>
-                <select name="gender" id="" className='form-control' onChange={getInputData} required>
-                  <option value="Select gender" >Select Gender</option>
+              <div className="col-md-6 mb-3">
+                <label htmlFor="gender">Gender <span><sup className="text-danger">*</sup></span></label>
+                <select name="gender" className="form-control" onChange={getInputData} required>
+                  <option value="Select gender">Select Gender</option>
                   <option value="Male">Male</option>
                   <option value="Female">Female</option>
                 </select>
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Date Of Birth <span><sup className='text-danger'>*</sup></span></label>
-                <input type="date" name='dateofbirth' className="form-control" onChange={getInputData} placeholder="DOB" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="dateofbirth">Date Of Birth <span><sup className="text-danger">*</sup></span></label>
+                <input type="date" name="dateofbirth" className="form-control" onChange={getInputData} placeholder="DOB" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Place Of Birth <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='birthplace' className="form-control" onChange={getInputData} placeholder="Birth Place" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="birthplace">Place Of Birth <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="birthplace" className="form-control" onChange={getInputData} placeholder="Birth Place" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Height <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='height' className="form-control" onChange={getInputData} placeholder="Height" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="height">Height <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="height" className="form-control" onChange={getInputData} placeholder="Height" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Siblings <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='siblings' className="form-control" onChange={getInputData} placeholder="Siblings" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="siblings">Siblings <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="siblings" className="form-control" onChange={getInputData} placeholder="Siblings" required />
               </div>
-            </div>
-            <div className="row g-3">
-              <div className="col mb-3">
-                <label htmlFor="">Religion <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='religion' className="form-control" onChange={getInputData} placeholder="Religion" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="religion">Religion <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="religion" className="form-control" onChange={getInputData} placeholder="Religion" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Cast <span><sup className='text-danger'>*</sup></span></label>
-                <select name="cast" id="" className='form-control' onChange={getInputData} required>
-                  <option value="Select gender" >Select Cast</option>
-                  <option value="Genral">Genral</option>
+              <div className="col-md-6 mb-3">
+                <label htmlFor="cast">Caste <span><sup className="text-danger">*</sup></span></label>
+                <select name="cast" className="form-control" onChange={getInputData} required>
+                  <option value="Select caste">Select Caste</option>
+                  <option value="General">General</option>
                   <option value="OBC">OBC</option>
                   <option value="SC">SC</option>
                   <option value="ST">ST</option>
                 </select>
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">SubCast <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='subcast' className="form-control" onChange={getInputData} placeholder="Sub Cast" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="subcast">Sub Caste <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="subcast" className="form-control" onChange={getInputData} placeholder="Sub Caste" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Self Gotra <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='gotra' className="form-control" onChange={getInputData} placeholder="Gotra" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="gotra">Self Gotra <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="gotra" className="form-control" onChange={getInputData} placeholder="Gotra" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Mother Gotra <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='mgotra' className="form-control" onChange={getInputData} placeholder="Mother Gotra" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="mgotra">Mother Gotra <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="mgotra" className="form-control" onChange={getInputData} placeholder="Mother Gotra" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Grand Mother Gotra <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='ggotra' className="form-control" onChange={getInputData} placeholder="Grand Mother Gotra" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="ggotra">Grandmother Gotra <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="ggotra" className="form-control" onChange={getInputData} placeholder="Grandmother Gotra" required />
               </div>
-            </div>
-            <div className="row g-3">
-              <div className="col mb-3">
-                <label htmlFor="">Educations <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='education' className="form-control" onChange={getInputData} placeholder="Education" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="education">Education <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="education" className="form-control" onChange={getInputData} placeholder="Education" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Workin Company Name</label>
-                <input type="text" name='companyname' className="form-control" onChange={getInputData} placeholder="Working Company Name" />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="companyname">Working Company Name</label>
+                <input type="text" name="companyname" className="form-control" onChange={getInputData} placeholder="Working Company Name" />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Anual Salary</label>
-                <input type="text" name='salary' className="form-control" onChange={getInputData} placeholder="Anual Salary" />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="salary">Annual Salary</label>
+                <input type="text" name="salary" className="form-control" onChange={getInputData} placeholder="Annual Salary" />
               </div>
-            </div>
-            <div className="row g-3">
-              <div className="col mb-3">
-                <label htmlFor="">Address <span><sup className='text-danger'>*</sup></span></label>
-                <textarea name="address" id="" onChange={getInputData} className='form-control' rows={1} placeholder='Address' required></textarea>
+              <div className="col-12 mb-3">
+                <label htmlFor="address">Address <span><sup className="text-danger">*</sup></span></label>
+                <textarea name="address" onChange={getInputData} className="form-control" rows={1} placeholder="Address" required></textarea>
               </div>
-            </div>
-            <div className="row g-3">
-              <div className="col mb-3">
-                <label htmlFor="">Pin <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='pin' className="form-control" onChange={getInputData} placeholder="Pin" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="pin">Pin <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="pin" className="form-control" onChange={getInputData} placeholder="Pin" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">City <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='city' className="form-control" onChange={getInputData} placeholder="city" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="city">City <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="city" className="form-control" onChange={getInputData} placeholder="City" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="state">State <span><sup className='text-danger'>*</sup></span></label>
+              <div className="col-md-6 mb-3">
+                <label htmlFor="state">State <span><sup className="text-danger">*</sup></span></label>
                 <select name="state" id="state" className="form-control" onChange={getInputData} required>
                   <option value="">Select State</option>
                   <option value="Andhra Pradesh">Andhra Pradesh</option>
@@ -241,35 +231,30 @@ const Signup = () => {
                   <option value="Puducherry">Puducherry</option>
                 </select>
               </div>
-
-            </div>
-            <div className="row g-3">
-              <div className="col mb-3">
-                <label htmlFor="">Email <span><sup className='text-danger'>*</sup></span></label>
-                <input type="email" name='email' className="form-control" onChange={getInputData} placeholder="Email" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="email">Email <span><sup className="text-danger">*</sup></span></label>
+                <input type="email" name="email" className="form-control" onChange={getInputData} placeholder="Email" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Phone Number <span><sup className='text-danger'>*</sup></span></label>
-                <input type="text" name='phone' className="form-control" onChange={getInputData} placeholder="Phone Number" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="phone">Phone Number <span><sup className="text-danger">*</sup></span></label>
+                <input type="text" name="phone" className="form-control" onChange={getInputData} placeholder="Phone Number" required />
               </div>
-              <div className="col mb-3">
-                <label htmlFor="">Password <span><sup className='text-danger'>*</sup></span></label>
-                <input type="password" name='password' className="form-control" onChange={getInputData} placeholder="Password" required />
+              <div className="col-md-6 mb-3">
+                <label htmlFor="password">Password <span><sup className="text-danger">*</sup></span></label>
+                <input type="password" name="password" className="form-control" onChange={getInputData} placeholder="Password" required />
               </div>
-            </div>
-            <div className="row g-3">
-              <div className="col mb-3">
-                <label htmlFor="">Image <span><sup className='text-danger'>*</sup></span></label>
-                <input type="file" name='image' className="form-control" onChange={getFileData} placeholder="Image" required />
+              <div className="col-12 mb-3">
+                <label htmlFor="image">Image <span><sup className="text-danger">*</sup></span></label>
+                <input type="file" name="image" className="form-control" onChange={getFileData} placeholder="Image" required />
               </div>
-            </div>
-            <div className="mb-3">
-              <button>Sign Up</button>
+              <div className="col-12 mb-3">
+                <button >Sign Up</button>
+              </div>
             </div>
           </form>
         </div>
-
       </div>
+
     </>
   )
 }
